d8659a4427c5f76f7b168a28bbd8212d71393e02
david <david.journo.31@gmail.com>
2018-06-16 16:29:30 +0200

    queries

--- requests.sql
+++ requests.sql
@@ -8,3 +8,14 @@ FROM client;
 SELECT title
 FROM film;
 
+--- afficher le film vu par la liste de clients
+SELECT movie_watched
+FROM client;
+
+--- afficher le film vu cette semaine par la liste de clients
+SELECT movie_watched_this_week
+FROM client;
+
+--- afficher le nombre de places disponibles d'une salle
+
+
