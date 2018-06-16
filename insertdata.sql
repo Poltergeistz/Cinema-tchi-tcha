934aa0f52ea7027c79f42ba015cbeb6abae8cbfd
marion1998 <marion281098@outlook.fr>
2018-06-16 15:28:34 +0200

    Ajout de film

--- insertdata.sql
+++ insertdata.sql
@@ -16,3 +16,9 @@ VALUES ('Django Unchained','Quentin Tarantino',2012,'Drama',245,'With the help o
 
 INSERT INTO film (title,director,released,genre,duration,synopsis)
 VALUES ('The Big Lebowski','Joel Cohen',1998,'Comedy', 157,'Jeffrey "The Dude" Lebowski, a Los Angeles slacker who only wants to bowl and drink white Russians, is mistaken for another Jeffrey Lebowski, a wheelchair-bound millionaire, and finds himself dragged into a strange series of events involving nihilists, adult film producers, ferrets, errant toes, and large sums of money.');
+
+INSERT INTO film (title,director,released,genre,duration,synopsis)
+VALUES ('Jumanji: Bienvenue dans la jungle','Jake Kasdan',2017,'Fantastique, Action', 159,'The tables are turned as four teenagers are sucked into Jumanji's world - pitted against rhinos, black mambas and an endless variety of jungle traps and puzzles. To survive, they'll play as characters from the game..');
+
+INSERT INTO film (title,director,released,genre,duration,synopsis)
+VALUES ('Flashdance','Adrian Lyne',1983,'Drame, Romance, Musical', 134,"The popular 1980’s dance movie that depicts the life of an exotic dancer with a side job as a welder who true desire is to get into ballet school. It’s her dream to be a professional dancer and now is her chance.…");
