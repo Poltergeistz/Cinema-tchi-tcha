da2eabf63ced17817f83876e6ac40000e518b679
david <david.journo.31@gmail.com>
2018-06-16 16:28:42 +0200

    duration time

--- insertdata.sql
+++ insertdata.sql
@@ -1,31 +1,31 @@
 -- FILM --
 
 INSERT film (title,director,released,genre,actor,duration,synopsis)
-VALUES ('Taxi Driver','Martin Scorsese',1976,'Drama','Robert De Niro',1:14,'A mentally unstable veteran works as a nighttime taxi driver in New York City, where the perceived decadence and sleaze fuels his urge for violent action, while attempting to liberate a twelve-year-old prostitute.' );
+VALUES ('Taxi Driver','Martin Scorsese',1976,'Drama','Robert De Niro',114,'A mentally unstable veteran works as a nighttime taxi driver in New York City, where the perceived decadence and sleaze fuels his urge for violent action, while attempting to liberate a twelve-year-old prostitute.' );
 
 INSERT film (title,director,released,genre,actor,duration,synopsis)
-VALUES ('Brazil','Terry Gilliam',1985,'Dystopia','Jonathan Pryce',1:42,'A bureaucrat, in a retro-future world, tries to correct an administrative error, and becomes an enemy of the state. ' );
+VALUES ('Brazil','Terry Gilliam',1985,'Dystopia','Jonathan Pryce',142,'A bureaucrat, in a retro-future world, tries to correct an administrative error, and becomes an enemy of the state. ' );
 
 INSERT film (title,director,released,genre,duration,synopsis)
-VALUES ('A Land Before Time','Don Bluth',1988,'Animation',0:69,'An orphaned brontosaurus named Littlefoot sets off in search of the legendary Great Valley. A land of lush vegetation where the dinosaurs can thrive and live in peace. Along the way he meets four other young dinosaurs, each one a different species, and they encounter several obstacles as they learn to work together in order to survive' );
+VALUES ('A Land Before Time','Don Bluth',1988,'Animation',069,'An orphaned brontosaurus named Littlefoot sets off in search of the legendary Great Valley. A land of lush vegetation where the dinosaurs can thrive and live in peace. Along the way he meets four other young dinosaurs, each one a different species, and they encounter several obstacles as they learn to work together in order to survive' );
 
 INSERT film (title,director,released,genre,duration,synopsis)
-VALUES ('The Lord Of The Rings : The Fellowship of the Ring','Peter Jackson',2001,'Fantasy',2:58,'Young hobbit Frodo Baggins, after inheriting a mysterious ring from his uncle Bilbo, must leave his home in order to keep it from falling into the hands of its evil creator. Along the way, a fellowship is formed to protect the ringbearer and make sure that the ring arrives at its final destination: Mt. Doom, the only place where it can be destroyed.' );
+VALUES ('The Lord Of The Rings : The Fellowship of the Ring','Peter Jackson',2001,'Fantasy',258,'Young hobbit Frodo Baggins, after inheriting a mysterious ring from his uncle Bilbo, must leave his home in order to keep it from falling into the hands of its evil creator. Along the way, a fellowship is formed to protect the ringbearer and make sure that the ring arrives at its final destination: Mt. Doom, the only place where it can be destroyed.' );
 
 INSERT film (title,director,released,genre,duration,synopsis)
-VALUES ('Django Unchained','Quentin Tarantino',2012,'Drama',2:45,'With the help of a German bounty hunter, a freed slave sets out to rescue his wife from a brutal Mississippi plantation owner.');
+VALUES ('Django Unchained','Quentin Tarantino',2012,'Drama',245,'With the help of a German bounty hunter, a freed slave sets out to rescue his wife from a brutal Mississippi plantation owner.');
 
 INSERT film (title,director,released,genre,duration,synopsis)
-VALUES ('The Big Lebowski','Joel Cohen',1998,'Comedy', 1:57,'Jeffrey "The Dude" Lebowski, a Los Angeles slacker who only wants to bowl and drink white Russians, is mistaken for another Jeffrey Lebowski, a wheelchair-bound millionaire, and finds himself dragged into a strange series of events involving nihilists, adult film producers, ferrets, errant toes, and large sums of money.');
+VALUES ('The Big Lebowski','Joel Cohen',1998,'Comedy', 157,'Jeffrey "The Dude" Lebowski, a Los Angeles slacker who only wants to bowl and drink white Russians, is mistaken for another Jeffrey Lebowski, a wheelchair-bound millionaire, and finds himself dragged into a strange series of events involving nihilists, adult film producers, ferrets, errant toes, and large sums of money.');
 
 INSERT film (title,director,released,genre,duration,synopsis)
 VALUES ('Jumanji: Bienvenue dans la jungle','Jake Kasdan',2017,'Fantastique, Action', 159,"The tables are turned as four teenagers are sucked into Jumanji's world - pitted against rhinos, black mambas and an endless variety of jungle traps and puzzles. To survive, they'll play as characters from the game...");
 
 INSERT film (title,director,released,genre,duration,synopsis)
-VALUES ('Flashdance','Adrian Lyne',1983,'Drame, Romance, Musical', 1:34,'The popular 1980’s dance movie that depicts the life of an exotic dancer with a side job as a welder who true desire is to get into ballet school. It’s her dream to be a professional dancer and now is her chance.');
+VALUES ('Flashdance','Adrian Lyne',1983,'Drame, Romance, Musical', 134,'The popular 1980’s dance movie that depicts the life of an exotic dancer with a side job as a welder who true desire is to get into ballet school. It’s her dream to be a professional dancer and now is her chance.');
 
 INSERT film (title,director,released,genre,duration,synopsis)
-VALUES ('Rocky Balboa','Sylvester Stallone',2006,'Drama', 1:42, "When he loses a highly publicized virtual boxing match to ex-champ Rocky Balboa, reigning heavyweight titleholder, Mason Dixon retaliates by challenging Rocky to a nationally televised, 10-round exhibition bout. To the surprise of his son and friends, Rocky agrees to come out of retirement and face an opponent who's faster, stronger and thirty years his junior.");
+VALUES ('Rocky Balboa','Sylvester Stallone',2006,'Drama', 142, "When he loses a highly publicized virtual boxing match to ex-champ Rocky Balboa, reigning heavyweight titleholder, Mason Dixon retaliates by challenging Rocky to a nationally televised, 10-round exhibition bout. To the surprise of his son and friends, Rocky agrees to come out of retirement and face an opponent who's faster, stronger and thirty years his junior.");
 
 -- Client --
 INSERT client (lastname,firstname,birthdate)
