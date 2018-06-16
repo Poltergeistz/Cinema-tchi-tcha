97b85f42d048b7d2ca80ffab106949d8648be3ab
david <david.journo.31@gmail.com>
2018-06-16 15:43:04 +0200

    add people to database

--- insertdata.sql
+++ insertdata.sql
@@ -1,18 +1,39 @@
-INSERT INTO film (title,director,released,genre,actor,duration,synopsis )
+-- FILM --
+
+INSERT film (title,director,released,genre,actor,duration,synopsis)
 VALUES ('Taxi Driver','Martin Scorsese',1976,'Drama','Robert De Niro',114,'A mentally unstable veteran works as a nighttime taxi driver in New York City, where the perceived decadence and sleaze fuels his urge for violent action, while attempting to liberate a twelve-year-old prostitute.' );
 
-INSERT INTO film (title,director,released,genre,actor,duration,synopsis )
+INSERT film (title,director,released,genre,actor,duration,synopsis)
 VALUES ('Brazil','Terry Gilliam',1985,'Dystopia','Jonathan Pryce',142,'A bureaucrat, in a retro-future world, tries to correct an administrative error, and becomes an enemy of the state. ' );
 
-INSERT INTO film (title,director,released,genre,duration,synopsis )
+INSERT film (title,director,released,genre,duration,synopsis)
 VALUES ('A Land Before Time','Don Bluth',1988,'Animation',69,'An orphaned brontosaurus named Littlefoot sets off in search of the legendary Great Valley. A land of lush vegetation where the dinosaurs can thrive and live in peace. Along the way he meets four other young dinosaurs, each one a different species, and they encounter several obstacles as they learn to work together in order to survive' );
 
-INSERT INTO film (title,director,released,genre,duration,synopsis)
+INSERT film (title,director,released,genre,duration,synopsis)
 VALUES ('The Lord Of The Rings : The Fellowship of the Ring','Peter Jackson',2001,'Fantasy',258,
 'Young hobbit Frodo Baggins, after inheriting a mysterious ring from his uncle Bilbo, must leave his home in order to keep it from falling into the hands of its evil creator. Along the way, a fellowship is formed to protect the ringbearer and make sure that the ring arrives at its final destination: Mt. Doom, the only place where it can be destroyed.' );
 
-INSERT INTO film (title,director,released,genre,duration,synopsis)
+INSERT film (title,director,released,genre,duration,synopsis)
 VALUES ('Django Unchained','Quentin Tarantino',2012,'Drama',245,'With the help of a German bounty hunter, a freed slave sets out to rescue his wife from a brutal Mississippi plantation owner.' );
 
-INSERT INTO film (title,director,released,genre,duration,synopsis)
+INSERT film (title,director,released,genre,duration,synopsis)
 VALUES ('The Big Lebowski','Joel Cohen',1998,'Comedy', 157,'Jeffrey "The Dude" Lebowski, a Los Angeles slacker who only wants to bowl and drink white Russians, is mistaken for another Jeffrey Lebowski, a wheelchair-bound millionaire, and finds himself dragged into a strange series of events involving nihilists, adult film producers, ferrets, errant toes, and large sums of money.');
+
+-- Client --
+INSERT client (last_name,first_name,birthdate,situation)
+VALUES('David','Journo',02/04/1991,'Apprenant');
+
+INSERT client (last_name,first_name,birthdate,situation)
+VALUES('Marion',);
+
+INSERT client (last_name,first_name,birthdate,situation)
+VALUES('');
+
+INSERT client (last_name,first_name,birthdate,situation)
+VALUES('Hans','Smith',06/15/1985,'Unemployed');
+
+INSERT client (last_name,first_name,birthdate,situation)
+VALUES('Jake','Hoffman',09/21/1997,'Student');
+
+INSERT client (last_name,first_name,birthdate,situation)
+VALUES('Francis','Brown',01/11/1955,'Retired');
