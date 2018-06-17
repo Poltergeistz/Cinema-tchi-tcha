c2d626bba37fff76024015690c57409f6388f02f
Simon Darmandieu <simon.darmandieu@gmail.com>
2018-06-17 19:05:31 +0200

    improve last query

--- requests.sql
+++ requests.sql
@@ -24,11 +24,13 @@ LEFT JOIN projection ON FKidprojection = idprojection
 WHERE WEEK(debut) <= WEEK(current_date()) 
 	AND WEEK(debut) >= WEEK(current_date())-1
 
---- savoir si une salle est pleine (plus de reservation possible)
+--- savoir si une salle est pleine  , c'est à dire si on ne peut plus reserver pour cette séance
+--- on ne s'intéresse donc que aux séances à venir, sinon la requête a moins de sens
+--- on peut enlever la condition sur le jour , pour obtenir les salles remplies, par le passé
 SELECT idroom,debut
 FROM ticket 
 LEFT JOIN projection ON FKidprojection = idprojection
 LEFT JOIN room ON FKidroom = idroom
-WHERE seat = capacity;
+WHERE seat = capacity AND debut >= NOW()
 
 
