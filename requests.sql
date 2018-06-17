fae6e919ed23a5e442d65f1c16677a882cb45925
Simon Darmandieu <simon.darmandieu@gmail.com>
2018-06-17 19:10:47 +0200

    minor fix

--- requests.sql
+++ requests.sql
@@ -9,12 +9,12 @@ SELECT title
 FROM film;
 
 --- récupérer la liste des clients qui ont vu un film donné
-SELECT DISTINCT lastname,firstname,title
+SELECT DISTINCT lastname,firstname
 FROM ticket 
 LEFT JOIN client ON FKidclient = idclient
 LEFT JOIN projection ON FKidprojection = idprojection
 LEFT JOIN film ON FKidfilm = idfilm
-WHERE title = 'Taxi Driver'
+WHERE title = 'Taxi Driver';
 
 --- récupérer la liste des clients qui ont vu un film cette semaine
 SELECT DISTINCT lastname,firstname
@@ -22,7 +22,7 @@ FROM ticket
 LEFT JOIN client ON FKidclient = idclient
 LEFT JOIN projection ON FKidprojection = idprojection
 WHERE WEEK(debut) <= WEEK(current_date()) 
-	AND WEEK(debut) >= WEEK(current_date())-1
+	AND WEEK(debut) >= WEEK(current_date())-1;
 
 --- savoir si une salle est pleine  , c'est à dire si on ne peut plus reserver pour cette séance
 --- on ne s'intéresse donc que aux séances à venir, sinon la requête a moins de sens
@@ -31,6 +31,6 @@ SELECT idroom,debut
 FROM ticket 
 LEFT JOIN projection ON FKidprojection = idprojection
 LEFT JOIN room ON FKidroom = idroom
-WHERE seat = capacity AND debut >= NOW()
+WHERE seat = capacity AND debut >= NOW();
 
 
