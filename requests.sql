58dd06806a9315bb3b4f884c55060236ef55a8ae
Simon Darmandieu <simon.darmandieu@gmail.com>
2018-06-17 18:54:27 +0200

    big data

--- requests.sql
+++ requests.sql
@@ -9,7 +9,7 @@ SELECT title
 FROM film;
 
 --- récupérer la liste des clients qui ont vu un film donné
-SELECT DISTINCT lastname,firstname
+SELECT DISTINCT lastname,firstname,title
 FROM ticket 
 LEFT JOIN client ON FKidclient = idclient
 LEFT JOIN projection ON FKidprojection = idprojection
@@ -25,7 +25,7 @@ WHERE WEEK(debut) <= WEEK(current_date())
 	AND WEEK(debut) >= WEEK(current_date())-1
 
 --- savoir si une salle est pleine (plus de reservation possible)
-SELECT idroom
+SELECT idroom,debut
 FROM ticket 
 LEFT JOIN projection ON FKidprojection = idprojection
 LEFT JOIN room ON FKidroom = idroom
