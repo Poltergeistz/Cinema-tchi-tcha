ce276d9129c2094fab6bf4e8ffe004efb3d8ca09
Simon Darmandieu <simon.darmandieu@gmail.com>
2018-06-17 10:14:45 +0200

    queries

--- requests.sql
+++ requests.sql
@@ -9,12 +9,20 @@ SELECT title
 FROM film;
 
 --- afficher le film vu par la liste de clients
-SELECT movie_watched
-FROM client;
+SELECT DISTINCT lastname,firstname
+FROM ticket 
+LEFT JOIN client ON FKidclient = idclient
+LEFT JOIN projection ON FKidprojection = idprojection
+LEFT JOIN film ON FKidfilm = idfilm
+WHERE title = 'Taxi Driver'
 
 --- afficher le film vu cette semaine par la liste de clients
-SELECT movie_watched_this_week
-FROM client;
+SELECT DISTINCT lastname,firstname
+FROM ticket 
+LEFT JOIN client ON FKidclient = idclient
+LEFT JOIN projection ON FKidprojection = idprojection
+WHERE WEEK(debut) <= WEEK(current_date()) 
+	AND WEEK(debut) >= WEEK(current_date())-1
 
 --- afficher le nombre de places disponibles d'une salle
 
